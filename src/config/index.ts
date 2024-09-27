af3fd6f067d3386f156237cd8ee115729c733610
Pedro Galembeck <galembeckpedro@gmail.com>
2024-09-27 06:32:44 -0300

    feat: create project (shadcn-portfolio) structure

---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+export const config = {
+  name: "Pedro Galembeck",
+  url: "https://galembeck.vercel.app",
+  description:
+    "Computer Engineer student at @PUC-Campinas. Passionate about the main web & mobile development technologies and always looking for new challenges.",
+};
---
+++ src/constants/index.ts
@@ -0,0 +1,23 @@
+export const personalData = {
+  name: "Pedro Galembeck",
+  initials: "PG",
+  location: "São Paulo, Brazil",
+  locationLink: "https://www.google.com/maps/place/sp",
+  description:
+    "Computer Engineer student at @PUC-Campinas. Passionate about the main web & mobile development technologies and always looking for new challenges.",
+  summary:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed dui accumsan, aliquam risus ac, pharetra massa. Sed convallis lectus vel ipsum scelerisque blandit. Ut commodo dolor fringilla ante consequat malesuada. Aenean sagittis tincidunt leo, nec efficitur ligula blandit a. Duis tellus erat, blandit id mauris eu, pulvinar fermentum mauris. Maecenas sed ipsum ac magna laoreet maximus. Aliquam nec libero vehicula, rhoncus leo ac, consequat dolor. Sed molestie justo ut tellus sodales pulvinar. Sed augue ante, fermentum ac volutpat eu, aliquet eget odio. Integer vestibulum at ipsum id porta.",
+  avatarUrl: "/assets/avatar.png",
+  skills: [
+    "React.js",
+    "Next.js",
+    "JavaScript",
+    "TypeScript",
+    "Node.js",
+    "Python",
+    "Java",
+    "C",
+    "Assembly",
+    "Lua",
+  ],
+};
